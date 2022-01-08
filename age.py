693962aefa0269ae021086c76935604574b58996
Grace Weng <zzz.aaa@msa.hinet.net>
2022-01-09 06:26:04 +0800

    finished age.py

---
+++ age.py
@@ -0,0 +1,18 @@
+driving = input('請問你有沒有開過車?')
+#讓程式再問完有沒有開過車就終止的寫法
+if driving != '有' and driving != '沒有':
+	print('只能輸入 有/沒有')
+	rasis SystemExit
+
+age = input('請問你的年齡?')
+age = int(age)
+if driving == '有':
+	if age >= 18:
+		print('你通過測驗了')
+	else:
+		print('奇怪 你怎麼會有開過車')
+else driving == '沒有'
+	if age >= 18:
+		print('你可以考駕照了阿,怎麼沒有去考')
+	else:
+		print('很好,再過幾年就可以考駕照了')
